5f9464f44781070f48ddf25e033ff1e1e3319c5c
unknown <pv9797@mail.ru>
2025-06-16 01:02:20 +0300

    Added lab works structure and files

---
+++ Lr1/Blinov_P_V_24ISz/Lab1(2)/Lab1(2)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_2_
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string str = "ucdyk:кпкпук"; //Инициализация строки
+            int colonIndex = str.IndexOf(':'); //Поиск индекса двоеточия
+
+            if (colonIndex != -1) //Проверка, найдено ли двоеточие
+            {
+                Console.WriteLine("Количество знаков перед двоеточием: " + colonIndex); //Двоеточие найдено
+            }
+            else
+            {
+                Console.WriteLine("В строке нет двоеточия."); //Двоеточие не найдено 
+
+            }
+            
+        }
+    }
+}
